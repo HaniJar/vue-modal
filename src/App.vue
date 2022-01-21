8f24825e2975bc3fbbca11c6adcb7f2381ab3b36
Haniah Jardien <jardienhaniah@gmail.com>
2022-01-21 10:28:58 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <h1>{{ title }}</h1>
+  <p>have a look now</p>
+  <teleport to=".modals" v-if="showModal">
+    <modal :theme="promotion" @close="toggleModal">
+      <template v-slot:links>
+        <a href="#">register</a>
+        <a href="#">find out more</a>
+      </template>
+      <h1>selling goods</h1>
+      <p>IT stuff</p>
+    </modal>
+  </teleport>
+
+  <teleport to=".modals" v-if="showModalTwo">
+    <modal @close="toggleModalTwo">
+      <h1>register for promotions</h1>
+      <p>Do not miss out on upcoming promo codes!</p>
+    </modal>
+  </teleport>
+  <button @click="toggleModal">info</button>
+  &nbsp;
+  <button @click="toggleModalTwo">register</button>
+</template>
+
+<script>
+import modal from "./components/modal.vue";
+export default {
+  name: "App",
+  components: {
+    modal,
+  },
+  data() {
+    return {
+      title: "title",
+      showModal: false,
+      showModalTwo: false,
+    };
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+    toggleModalTwo() {
+      this.showModalTwo = !this.showModalTwo;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: rgb(0, 0, 0);
+  margin-top: 60px;
+}
+</style>
